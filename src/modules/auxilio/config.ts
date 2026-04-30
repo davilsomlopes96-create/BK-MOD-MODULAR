3ca889aa4c66977689c2816cb75d92651fcef8c2
davilsomlopes96-create <davilsomlopes96@gmail.com>
2026-04-30 19:14:09 -0300

    Create config.ts

---
+++ src/modules/auxilio/config.ts
@@ -0,0 +1,9 @@
+export const BK_Config = {
+  nome: "BK 100% HS Functional",
+  offsets: {
+    head: "0x5F80920", // Extraído da IMG_1149
+    lookAt: "0x672F4FC" // Extraído da IMG_1149
+  },
+  versao: "1.123.1",
+  data_atualizacao: "Abril/2026"
+};
